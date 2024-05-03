8edd015d939d374d70a6fad47b8f501dd18c7d1e
Doosan <doosan@hartmann-controls.com>
2024-05-03 08:03:13 -0400

    Instead of hard-coding the initial universe, generate a random one, where each cell has a fifty-fifty chance of being alive or dead.

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                if rand::random::<bool>() {
                     Cell::Alive
                 } else {
                     Cell::Dead

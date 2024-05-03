11f9b3ad04e69ac1be9281e39c0c264529240673
Doosan <doosan@hartmann-controls.com>
2024-05-03 19:51:40 -0400

    use fixedbitset to save memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use fixedbitset::FixedBitSet;
 
 #[wasm_bindgen]
 #[repr(u8)]
@@ -14,7 +15,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 #[wasm_bindgen(js_class=Universe)]
 impl Universe {
@@ -22,16 +23,12 @@ impl Universe {
     pub fn new() -> Universe {
         let width = 64;
         let height = 64;
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if rand::random::<bool>() {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
 
         Universe {
             width,
@@ -40,37 +37,26 @@ impl Universe {
         }
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
-    }
 
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
+        
 
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
+                
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
+                next.set(idx, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise
+                });
 
-                next[idx] = next_cell;
             }
         }
 
@@ -105,24 +91,9 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const usize {
+        self.cells.as_slice().as_ptr()
     }
     
 }
 
-use std::fmt;
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-
-        Ok(())
-    }
-}
--- www/index.js
+++ www/index.js
@@ -35,9 +35,14 @@ const drawGrid = () => {
 const getIndex = (row, column) => {
   return row * width + column;
 };
+const bitIsSet = (n, arr) => {
+  const byte = Math.floor(n / 8);
+  const mask = 1 << (n % 8);
+  return (arr[byte] & mask) === mask;
+};
 const drawCells = () => {
   const cellsPtr = universe.cells();
-  const cells = new Uint8Array(wasm.memory.buffer, cellsPtr, width * height);
+  const cells = new Uint8Array(wasm.memory.buffer, cellsPtr, width * height/8);
 
   ctx.beginPath();
 
@@ -45,9 +50,9 @@ const drawCells = () => {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOR
-        : ALIVE_COLOR;
+      ctx.fillStyle = bitIsSet(idx, cells)
+        ? ALIVE_COLOR
+        : DEAD_COLOR;
 
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
